4cd9ab0d9b6823acb1849e4d538814a3548a47d0
Akshay Suthar <akshay.suthar@gmail.com>
2023-04-19 16:49:25 -0400

    adding python packages in workflow

--- add_frontmatter.py
+++ add_frontmatter.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python
 import os
 import re
-import sys
 import warnings
 from datetime import datetime
-from pprint import pprint
 
 from frontmatter import Frontmatter
 
 warnings.filterwarnings('ignore')
 
-def edit():
-	for mdfile in os.listdir('_posts/'):
-		path = f"./_posts/{mdfile}"
-		date = datetime.fromtimestamp(os.stat(path).st_ctime).strftime("%Y-%m-%d %H:%M:%S -0400")
-		post = Frontmatter.read_file(path)
-		
-		if not post.get('frontmatter'):
-			filename = path.split('/')[-1].split('.')[0].split('-')
-			title_str = ' '.join(filename[3:]).capitalize()
 
-			yaml = f"---\ntitle: {title_str}\nlayout: post\ndate: {date}\ncategories: data\ntags: jekyll update\n---\n\n"
-			with open(path, 'r') as file:
-				filedata = file.read()
-				filedata = re.sub(r"!\[png\]\(", "<img src=\"/assets/images/", filedata)
-				filedata = re.sub(".png\)", ".png\">", filedata)
-				filedata = yaml + filedata
-				with open(path, 'w') as file:
-					file.write(filedata)
+def edit():
+    for mdfile in os.listdir('_posts/'):
+        path = f"./_posts/{mdfile}"
+        date = datetime.fromtimestamp(os.stat(path).st_ctime).strftime("%Y-%m-%d %H:%M:%S -0400")
+        post = Frontmatter.read_file(path)
+
+        if not post.get('frontmatter'):
+            filename = path.split('/')[-1].split('.')[0].split('-')
+            title_str = ' '.join(filename[3:]).capitalize()
+
+            yaml = f"---\ntitle: {title_str}\nlayout: post\ndate: {date}\ncategories: data\ntags: jekyll update\n---\n\n"
+            with open(path, 'r') as file:
+                filedata = file.read()
+                filedata = re.sub(r"!\[png\]\(", "<img src=\"/assets/images/", filedata)
+                filedata = re.sub(r".png\)", ".png\">", filedata)
+                filedata = yaml + filedata
+                with open(path, 'w') as file:
+                    file.write(filedata)
+
 
 if __name__ == '__main__':
-	edit()
+    edit()
